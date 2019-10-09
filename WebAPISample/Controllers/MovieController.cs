3ff592e9f8e7e2123894b651c5b1a4dee86603f6
gregmanthey <gmanthey03@gmail.com>
2019-10-09 10:32:02 -0500

    Changed return type of Get(id), Post, Delete(id) to IHttpActionResult and added try/catch statements to them to handle any currently unhandled exceptions.

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -8,7 +8,6 @@ using WebAPISample.Models;
 
 namespace WebAPISample.Controllers
 {
-    //[Route("api/Movies")]
     public class MovieController : ApiController
     {
         ApplicationDbContext db;
@@ -17,8 +16,6 @@ namespace WebAPISample.Controllers
             db = new ApplicationDbContext();
         }
         // GET api/values
-        //[HttpGet]
-        //[Route("GetMovies")]
         public IEnumerable<Movie> Get()
         {
             // Retrieve all movies from db logic
@@ -27,19 +24,47 @@ namespace WebAPISample.Controllers
         }
 
         // GET api/values/5
-        public Movie Get(int id)
+        public IHttpActionResult Get(int id)
         {
             // Retrieve movie by id from db logic
-            var movie = db.Movies.Find(id);
-            return movie;
+            try
+            {
+                var movie = db.Movies.Find(id);
+                return Ok(movie);
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+            
         }
 
         // POST api/values
-        public void Post([FromBody]Movie value)
+        public IHttpActionResult Post([FromBody]Movie value)
         {
             // Create movie in db logic
-            db.Movies.Add(value);
-            db.SaveChanges();
+            try
+            {
+                var movieInDb = db.Movies.SingleOrDefault(m => m.Title == value.Title && m.Director == value.Director);
+                if (movieInDb is null)
+                {
+                    db.Movies.Add(value);
+                    db.SaveChanges();
+                    var newMovie = db.Movies.SingleOrDefault(m => m.Title == value.Title && m.Director == value.Director);
+                    return Content(HttpStatusCode.Created, newMovie);
+                }
+                else
+                {
+                    return Content(HttpStatusCode.Found, Get(movieInDb.MovieId));
+                }
+            }
+            catch (Exception)
+            {
+
+                return InternalServerError(new Exception("Error, unable to create new row in database from supplied data"));
+            }
+            
         }
 
         // PUT api/values/5
@@ -54,12 +79,21 @@ namespace WebAPISample.Controllers
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
             // Delete movie from db logic
-            var deleteMovie = db.Movies.Find(id);
-            db.Movies.Remove(deleteMovie);
-            db.SaveChanges();
+            try
+            {
+                var deleteMovie = db.Movies.Find(id);
+                db.Movies.Remove(deleteMovie);
+                db.SaveChanges();
+                return Ok($"Movie at id {id} removed. Movie title was {deleteMovie.Title}");
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
         }
     }
 
